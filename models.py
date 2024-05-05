1cdeded94417fb01c1187a8796a6184ef7f61653
f1delius <tagazeem@gmail.com>
2024-05-05 16:25:00 +0530

    update Models

--- models.py
+++ models.py
@@ -13,14 +13,6 @@ class Item(BaseModel):
     publish: bool
     type: str
 
-class Users(BaseModel):
-    email_id: str
-    bot_username: str
-    name: str
-
-class UserCreate(BaseModel):
-    username: str
-
 class AgentUpdate(BaseModel):
     name: Optional[str]
     desc: Optional[str] = Field(alias="description")
@@ -41,21 +33,10 @@ class Bots(BaseModel):
     prompt: Optional[str]
     llmModel: Optional[str]
 
-class User(BaseModel):
-    id: str
-    bot_username: str
-    desc: str
-    name: str
-    tags: list
-    type: str
-    avatar_mxc : Optional[str]
-    prompt: Optional[str]
-    llmModel: Optional[str]
-
 class Agent(BaseModel):
     username: str
-    email_id: Optional[str]
     agent_id: str
+    owner_id: Optional[str]
 
 
 
